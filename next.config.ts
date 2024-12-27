75976a4c6805cc06ff04d769043dd22d7392c343
Baran Semih Güzel <103120449+bsg2101@users.noreply.github.com>
2024-12-28 01:32:49 +0300

    updated package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+    output: 'export',  // Statik export için
+    images: {
+      unoptimized: true, // Statik export için gerekli
+    },
 }
 
 module.exports = nextConfig
